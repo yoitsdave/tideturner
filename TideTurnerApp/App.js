eb02b0d3d0529d5addaeb00b86578f7906ad3f1b
Sangwon-Kwon01 <sangwon.kwon0854@gmail.com>
2023-03-29 14:39:57 -0400

    WORKING BUTTON N STUFF

--- TideTurnerApp/App.js
+++ TideTurnerApp/App.js
@@ -1,22 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './components/Button';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View} from "react-native";
 
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require("./assets/images/app-icon-all.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>START HERE!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
+  // Styles that are unchanged from previous step are hidden for brevity. 
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
