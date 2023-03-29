50aa3a8daa71466e422bf634eac2d1bc21f5e5cd
najaeyoung <lna1@terpmail.umd.edu>
2023-03-29 14:51:58 -0400

    create components

--- TideTurnerApp/App.js
+++ TideTurnerApp/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
+
 
 export default function App() {
   return (
