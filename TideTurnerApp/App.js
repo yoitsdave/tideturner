a615a6911bd6774523dbd2581703f56e7bd59910
najaeyoung <lna1@terpmail.umd.edu>
2023-04-03 14:54:31 -0400

    Implement SetupFilter through App.js

--- TideTurnerApp/App.js
+++ TideTurnerApp/App.js
@@ -1,30 +1,153 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
+import React, { useState, useCallback } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import Header from "./components/Header";
+import {useForm, Controller} from 'react-hook-form';
 
-import Button from './components/Button';
-import ImageViewer from './components/ImageViewer';
+const Sign = () => {
+  const [genderOpen, setGenderOpen] = useState(false);
+  const [genderValue, setGenderValue] = useState(null);
+  const [gender, setGender] = useState([
+    { label: "Chlora Ball", value: "male" },
+    { label: "In-Line Filter", value: "female" },
+    { label: "Carbon Filter", value: "neutral" },
+    { label: "Reverse Osmosis Filter", value: "neutral" },
 
-const PlaceholderImage = require("./assets/images/app-icon-all.png");
+  ]);
+  const [companyOpen, setCompanyOpen] = useState(false);
+  const [companyValue, setCompanyValue] = useState(null);
+  const [company, setComapny] = useState([
+    { label: "Chlora Ball", value: "male" },
+    { label: "In-Line Filter", value: "female" },
+    { label: "Carbon Filter", value: "neutral" },
+    { label: "Reverse Osmosis Filter", value: "neutral" },
+  ]);
+  const [loading, setLoading] = useState(false);
+  const onGenderOpen = useCallback(() => {
+    setCompanyOpen(false);
+  }, []);
 
-export default function App() {
+  const onCompanyOpen = useCallback(() => {
+    setGenderOpen(false);
+  }, []);
+  const { handleSubmit, control } = useForm();
+  const onSubmit = (data) => {
+    console.log(data, "data");
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      
+
+      
+      <View>
+
+
+
+      <Text style={styles.label}>Name Of Filter</Text>
+      <Controller
+        name="company"
+        defaultValue=""
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <View style={styles.dropdownCompany}>
+            <DropDownPicker
+              style={styles.dropdown}
+              open={companyOpen}
+              value={companyValue} //companyValue
+              items={company}
+              setOpen={setCompanyOpen}
+              setValue={setCompanyValue}
+              setItems={setComapny}
+              placeholder="Select Filter"
+              placeholderStyle={styles.placeholderStyles}
+              loading={loading}
+              activityIndicatorColor="#5188E3"
+              searchable={true}
+              searchPlaceholder="Search your filter here..."
+              onOpen={onCompanyOpen}
+              onChangeValue={onChange}
+               zIndex={1000}
+              zIndexInverse={3000}
+            />
+          </View>
+        )}
+      />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
+   
+
+
+      <TouchableOpacity onPress={handleSubmit(onSubmit)}>
+        <Text style={styles.getStarted}>Get Started</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity style={styles.logIn}>
+        <Text style={styles.links}>I have an account</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  // Styles that are unchanged from previous step are hidden for brevity. 
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
+  container: {
+    flex: 1,
+  },
+  input: {
+    borderStyle: "solid",
+    borderColor: "#B7B7B7",
+    borderRadius: 7,
+    borderWidth: 1,
+    fontSize: 15,
+    height: 50,
+    marginHorizontal: 10,
+    paddingStart: 10,
+    marginBottom: 15,
+  },
+  label: {
+    marginBottom: 7,
+    marginStart: 10,
+  },
+  placeholderStyles: {
+    color: "grey",
+  },
+  dropdownGender: {
+    marginHorizontal: 10,
+    width: "50%",
+    marginBottom: 15,
+  },
+  dropdownCompany: {
+    marginHorizontal: 10,
+    marginBottom: 15,
+  },
+  dropdown: {
+    borderColor: "#B7B7B7",
+    height: 50,
+  },
+  getStarted: {
+    backgroundColor: "#5188E3",
+    color: "white",
+    textAlign: "center",
+    marginHorizontal: 60,
+    paddingVertical: 15,
+    borderRadius: 50,
+    marginTop: 20,
+  },
+  logIn: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 10,
+  },
+  links: {
+    textAlign: "center",
+    textDecorationLine: "underline",
+    color: "#758580",
   },
 });
+
+export default Sign;
