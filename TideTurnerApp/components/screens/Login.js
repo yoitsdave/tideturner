a7e696f03e85954b618cb88f849c8033bc88c741
JJL <levyjj1312@gmail.com>
2023-04-12 15:51:46 -0400

    Skeleton Login and Register Screens

--- TideTurnerApp/components/screens/Login.js
+++ TideTurnerApp/components/screens/Login.js
@@ -1,6 +1,66 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Button, View } from 'react-native';
+
+import { getTextInput } from '../Input';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 20,
+    },
+    label: {
+      fontSize: 16,
+      marginRight: 10,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#999',
+      borderRadius: 5,
+      height: 40,
+      width: 200,
+      paddingHorizontal: 10,
+      fontSize: 16,
+    },
+});
+  
+const TextInput = getTextInput(styles);
+
+const logInUser = (username, password) => {
+    alert("logging in user with name " + username + " and password " + password);
+}
 
 export default LoginScreen = ({navigation}) => {
-    return <Text> Login Screen not implemented </Text>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    return (
+        <View style={styles.container}>
+          <TextInput
+            label="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+    
+          <TextInput
+            label="Password"
+            value={password}
+            secureTextEntry={true}
+            onChangeText={setPassword}
+          />
+    
+          <Button
+            title="Log In"
+            onPress={() => {
+                logInUser(username, password);
+            }}
+          />
+    
+        </View>
+    );
 }
--- TideTurnerApp/components/screens/Register.js
+++ TideTurnerApp/components/screens/Register.js
@@ -1,6 +1,65 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Button, View } from 'react-native';
+
+import { getTextInput } from '../Input';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 20,
+    },
+    label: {
+      fontSize: 16,
+      marginRight: 10,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#999',
+      borderRadius: 5,
+      height: 40,
+      width: 200,
+      paddingHorizontal: 10,
+      fontSize: 16,
+    },
+});
+  
+const TextInput = getTextInput(styles);
+
+const registerUser = (username, password) => {
+    alert("registering user with name " + username + " and password " + password);
+}
 
 export default RegisterScreen = ({navigation}) => {
-    return <Text> Register Screen not implemented </Text>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    return (
+        <View style={styles.container}>
+          <TextInput
+            label="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+    
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+          />
+    
+          <Button
+            title="Register"
+            onPress={() => {
+              registerUser(username, password);
+            }}
+          />
+    
+        </View>
+    );
 }
